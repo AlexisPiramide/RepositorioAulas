8b887ba03c4fd7f31250d2ed26bd931772cee087
Alexis <220240@fppiramide.com>
2023-10-09 09:00:52 +0200

    Primer Commit

---
+++ src/main/java/com/example/RepositorioAulas/RepositorioAulasApplication.java
@@ -0,0 +1,13 @@
+package com.example.RepositorioAulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RepositorioAulas/RepositorioAulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RepositorioAulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioAulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
