ef0ad31653dffff9f8511247fd84eadafa7d4232
Alexis <220240@fppiramide.com>
2023-10-11 09:41:20 +0200

    Formulario para elegir aula completo

--- src/main/java/com/example/RepositorioAulas/BD/AulaDAO.java
+++ src/main/java/com/example/RepositorioAulas/BD/AulaDAO.java
@@ -1,5 +1,7 @@
 package com.example.RepositorioAulas.BD;
 
+import com.example.RepositorioAulas.Aula;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -17,8 +19,8 @@ public class AulaDAO {
 
     public static List<String> obtenerAulas() {
 
-        String id = "id";
-        String query = "SELECT " + id + " FROM aulas";
+        String id = "id_aula";
+        String query = "SELECT " + id + " FROM relacion;";
         List<String> aulas = new ArrayList<>();
 
         try (PreparedStatement statement = conexion.prepareStatement(query);
@@ -28,6 +30,39 @@ public class AulaDAO {
                 String aula = resultSet.getString(id);
                 aulas.add(aula);
             }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return aulas;
+    }
+
+    public static List<Aula> obtenerhorario() {
+
+
+        String IDAula = "";
+        ;
+        String query = "SELECT * FROM aulas where idclase='"+IDAula+"';";
+
+
+        List<Aula> aulas = new ArrayList<>();
+
+        try (PreparedStatement statement = conexion.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                Integer sesion = resultSet.getInt("sesion");
+                String dia = resultSet.getString("dia");
+                Integer horainicio = resultSet.getInt("horainicio");
+                Integer horafin = resultSet.getInt("horafin");
+                String profesor = resultSet.getString("profesor");
+                String materia = resultSet.getString("materia");
+
+                Aula aula = new Aula(IDAula,dia,profesor,materia,sesion,horainicio,horafin);
+                aulas.add(aula);
+            }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
