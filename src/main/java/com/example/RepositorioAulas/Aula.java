98d6e71fc504ee4f2c37df1d56d3cbefd31ebc51
Alexis <220240@fppiramide.com>
2023-10-11 13:14:01 +0200

    Tablas creadas

--- src/main/java/com/example/RepositorioAulas/Aula.java
+++ src/main/java/com/example/RepositorioAulas/Aula.java
@@ -2,44 +2,72 @@ package com.example.RepositorioAulas;
 
 public class Aula {
 
-    private String idAula,dia,profesor,materia;
-    private Integer nºSesion,horaInicio,horaFin;
+    private String id_aula,dia,profesor, id_asignatura;
+    private Integer id_sesion, hora_inicio, hora_fin;
 
-    public Aula(String idAula, String dia, String profesor, String materia, Integer nºSesion, Integer horaInicio, Integer horaFin) {
-        this.idAula = idAula;
+    public Aula(String idAula, String dia, String profesor, String id_asignatura, Integer id_sesion, Integer hora_inicio, Integer hora_fin) {
+        this.id_aula = idAula;
         this.dia = dia;
         this.profesor = profesor;
-        this.materia = materia;
-        this.nºSesion = nºSesion;
-        this.horaInicio = horaInicio;
-        this.horaFin = horaFin;
+        this.id_asignatura = id_asignatura;
+        this.id_sesion = id_sesion;
+        this.hora_inicio = hora_inicio;
+        this.hora_fin = hora_fin;
     }
 
-    public String getIdAula() {
-        return idAula;
+    public String getId_aula() {
+        return id_aula;
+    }
+
+    public void setId_aula(String id_aula) {
+        this.id_aula = id_aula;
     }
 
     public String getDia() {
         return dia;
     }
 
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
     public String getProfesor() {
         return profesor;
     }
 
-    public String getMateria() {
-        return materia;
+    public void setProfesor(String profesor) {
+        this.profesor = profesor;
     }
 
-    public Integer getNºSesion() {
-        return nºSesion;
+    public String getId_asignatura() {
+        return id_asignatura;
     }
 
-    public Integer getHoraInicio() {
-        return horaInicio;
+    public void setId_asignatura(String id_asignatura) {
+        this.id_asignatura = id_asignatura;
     }
 
-    public Integer getHoraFin() {
-        return horaFin;
+    public Integer getId_sesion() {
+        return id_sesion;
+    }
+
+    public void setId_sesion(Integer id_sesion) {
+        this.id_sesion = id_sesion;
+    }
+
+    public Integer getHora_inicio() {
+        return hora_inicio;
+    }
+
+    public void setHora_inicio(Integer hora_inicio) {
+        this.hora_inicio = hora_inicio;
+    }
+
+    public Integer getHora_fin() {
+        return hora_fin;
+    }
+
+    public void setHora_fin(Integer hora_fin) {
+        this.hora_fin = hora_fin;
     }
 }
